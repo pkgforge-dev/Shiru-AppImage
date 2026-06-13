ed6bcbfaf3b19e59324879457588387abc66b5df
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-13 15:20:32 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm libnss_nis nss-mdns nss xdotool
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -17,10 +17,24 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # If the application needs to be manually built that has to be done down here
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+echo "Getting binary..."
+echo "---------------------------------------------------------------"
+# Upstream only has a single deb that is x86_64 and has no arch in it
+#case "$ARCH" in
+#	x86_64)  farch=amd64;;
+#	aarch64) farch=arm64;;
+#esac
+link=$(wget https://api.github.com/repos/RockinChaos/Shiru/releases/latest -O - \
+	| sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*/linux-Shiru-.*.deb")
+wget --retry-connrefused --tries=30 "$link" -O /tmp/temp.deb
+ar x /tmp/temp.deb
+tar -xvf ./data.tar.xz
+rm -f ./*.tar.xz /tmp/temp.deb
+
+mkdir -p ./AppDir/bin
+cp -rv ./opt/Shiru/*                                   ./AppDir/bin
+cp -v ./usr/share/applications/shiru.desktop           ./AppDir
+cp -v ./usr/share/icons/hicolor/512x512/apps/shiru.png ./AppDir 
+cp -v ./usr/share/icons/hicolor/512x512/apps/shiru.png ./AppDir/.DirIcon
+
+echo "$link" | awk -F'/' '{print $(NF-1)}' > ~/version
